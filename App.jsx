2609a19d360d13e3759a0d961217b25e64b472ca
Daniel <truenorthtrainingcompany@gmail.com>
2016-05-09 18:03:00 -0400

    Create Resolution components ResolutionsForm and ResolutionSingle to take in and display resolutions

--- App.jsx
+++ App.jsx
@@ -1,35 +1,26 @@
 import React from 'react';
 import TrackerReact from 'meteor/ultimatejs:tracker-react';
 
+import ResolutionsForm from './ResolutionsForm.jsx';
+import ResolutionSingle from './ResolutionSingle.jsx';
+
 Resolutions = new Mongo.Collection("resolutions");
 
 export default class App extends TrackerReact(React.Component) {
+	
 	resolutions() {
-		return Resolutions.find().fetch
-	}
-	addResolution(event) {
-		event.preventDefault();
-		var text = this.refs.resolution.value.trim();
-		
-		Resolutions.insert({
-			text: text,
-			complete: false,
-			createdAt: new Date()
-		});
-		
-		this.refs.resolution.value = "";
+		return Resolutions.find().fetch();
 	}
 	render() {
-		let res = this.resolutions();
 		return(
 			<div>
 				<h1>My Resolutions</h1>
-				<form className="new-resolution" onSubmit={this.addResolution.bind(this)}>
-					<input
-						type="text"
-						ref="resolution"
-						placeholder="Finish React Meteor Series" />
-				</form>
+				<ResolutionsForm />
+				<ul>
+				{this.resolutions().map( (resolution)=>{
+					return <ResolutionSingle resolution={resolution} />
+				})}
+				</ul>
 			</div>
 		)
 	}
