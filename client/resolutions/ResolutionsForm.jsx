682cddc37b00063fb1c9d1fdea60973cae26385a
Daniel <truenorthtrainingcompany@gmail.com>
2016-05-11 16:08:16 -0400

    Remove insecure, add meteor methods Insecure package removed, methods.js added to /server addResolution is placed inside of Meteor.Methods and this method is called from the ResolutionsForm component, with an arrow function to ensure the input field is not cleared unless insert successful

--- client/resolutions/ResolutionsForm.jsx
+++ client/resolutions/ResolutionsForm.jsx
@@ -4,14 +4,11 @@ export default class ResolutionsForm extends Component {
 	addResolution(event) {
 		event.preventDefault();
 		var text = this.refs.resolution.value.trim();
-		
-		Resolutions.insert({
-			text: text,
-			complete: false,
-			createdAt: new Date()
+
+		Meteor.call('addResolution', text, ()=>{
+			this.refs.resolution.value = "";
 		});
 		
-		this.refs.resolution.value = "";
 	}
 	render() {
 		return (
