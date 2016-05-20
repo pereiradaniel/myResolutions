d489db13bb3f08020e795859b0599910c88f13b1
Daniel <truenorthtrainingcompany@gmail.com>
2016-05-20 16:20:01 -0400

    Set resolution to complete Add input field to ResolutionSingle component Use Meteor method to update the document in the collection

--- client/resolutions/ResolutionSingle.jsx
+++ client/resolutions/ResolutionSingle.jsx
@@ -1,9 +1,16 @@
 import React, {Component} from 'react';
 
 export default class ResolutionSingle extends Component {
+	toggleChecked() {
+		Meteor.call('toggleResolution', this.props.resolution._id, this.props.resolution.complete);
+	}
 	render() {
 		return (
 			<li>
+				<input type="checkbox"
+					readOnly={true}
+					checked={this.props.resolution.complete}
+					onClick={this.toggleChecked.bind(this)} />
 				{this.props.resolution.text}
 				{this.props.resolution.complete.toString()}
 			</li>
--- server/methods.js
+++ server/methods.js
@@ -5,5 +5,10 @@ Meteor.methods({
 			complete: false,
 			createdAt: new Date()
 		});
+	},
+	toggleResolution(id, status) {
+		Resolutions.update(id, {
+			$set: {complete: !status}
+		});
 	}
 });
