62d0507de4aa2cd1951f6d67d73d875e42ae3ca3
Daniel <truenorthtrainingcompany@gmail.com>
2016-05-27 20:30:09 -0400

    Sign up button on about page Use meteor session variable for loginbuttons, button sets to true Add session package to meteor, setVar function toggles session variable to reveal sign up buttons

--- client/About.jsx
+++ client/About.jsx
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 
 export default class About extends Component {
+	setVar() {
+		Session.set('Meteor.loginButtons.dropdownVisible', true);
+	}
 	render() {
 		return (
 			<div>
 				<h1>About Us</h1>
 				<p>Ipsum lorem</p>
+				<button onClick={this.setVar}>Sign Up!</button>
 			</div>
 			)
 	}
